42b6064913df2989af88f107eea4fc780cdd0ac4
Volodymyr Shpylka <volodymyr.shpylka@aitastic.com>
2023-07-05 15:18:18 +0200

    make vsc_repo block dynamic

--- azure/tfc-workspace.tf
+++ azure/tfc-workspace.tf
@@ -14,6 +14,13 @@ resource "tfe_workspace" "my_workspace" {
   organization = var.tfc_organization_name
   project_id   = var.tfc_project_id
 
+  dynamic "vcs_repo" {
+    for_each = var.tfc_vcs_repo_object
+    content {
+      identifier = vcs_repo.value["identifier"]
+    }
+  }
+  
   vcs_repo {
     identifier = var.tfc_vcs_repo_identifier
   }
--- azure/vars.tf
+++ azure/vars.tf
@@ -49,8 +49,8 @@ variable "invoice_section_id" {
   description = "The invoice section id for the billing scope under which the created Subscription will be billed"
 }
 
-variable "tfc_vcs_repo_identifier" {
-  type        = string
+variable "tfc_vcs_repo_block" {
+  type        = object()
   default     = null
   description = "Identifier of the repo to be connected to the new workspace. org/repo-name format for github. Otherwise refer to terraform documentation of the tfe provider"
 }
